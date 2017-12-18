0cbe4b60cb3b1698cb54eb193c6b8abdc42d024c
Michael Sawyer <michaelsawyer92@gmail.com>
2017-12-17 22:10:13 -0800

    Adds gossip communication.

--- api/gossip/gossip.go
+++ api/gossip/gossip.go
@@ -3,23 +3,72 @@ package gossip
 import (
 	"net/http"
 	"encoding/json"
+	"strconv"
+	"bytes"
+	"fmt"
+	"math/rand"
 )
+
 
 type peer struct {
 	Port int `json:"port"`
+	State state `json:"state"`
+}
+
+type state struct {
+	Movie string `json:"movie"`
+	Version int `json:"version"`
 }
 
 var myPeers []peer
+var movies []string
+var selfState peer
 
-func Initialize(peerPort int) {
-	p := peer{
-		Port: peerPort,
+func Initialize(selfPort int, peerPort int) {
+	selfState = peer{
+		Port: selfPort,
+		State: state{
+			Movie: "Get Out",
+			Version: 1,
+		},
 	}
-	myPeers = append(make([]peer,0), p)
+
+	myPeers = make([]peer, 0)
+
+	if peerPort != 0 {
+		p := peer{
+			Port: peerPort,
+		}
+		myPeers = append(myPeers, p)
+	}
+
+	movies = []string {
+		"Master and Commander",
+		"Get Out",
+		"National Lampoon's Christmas Vacation",
+		"Star Wars: The Last Jedi",
+	}
+}
+
+func ChangeMovie () {
+	selfState.State.Movie = movies[rand.Intn(len(movies))]
+	selfState.State.Version++
 }
 
 func Gossip () {
-
+	for p := range myPeers {
+		peerUrl := "http://localhost:" + strconv.Itoa(myPeers[p].Port) +
+			"/gossip"
+		b := new(bytes.Buffer)
+		json.NewEncoder(b).Encode(append(myPeers, selfState))
+		resp, err := http.Post(peerUrl, "application/json", b)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		var otherPeers []peer
+		json.NewDecoder(resp.Body).Decode(&otherPeers)
+		updatePeers(otherPeers)
+	}
 }
 
 func HandleGossip (w http.ResponseWriter, r *http.Request) {
@@ -32,22 +81,29 @@ func HandleGossip (w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "Invalid Request Body. Please send peers.", 400)
 		} else {
 			updatePeers(otherPeers)
-			json.NewEncoder(w).Encode(myPeers)
+			json.NewEncoder(w).Encode(append(myPeers, selfState))
 		}
 	default:
 		http.Error(w, "Invalid Request Method", 400)
 	}
 }
 
+func PrintPeers () {
+	fmt.Println(append(myPeers, selfState))
+}
+
 func updatePeers (otherPeers []peer) {
 	for p := range otherPeers {
 		exists := false
 		for myPeer := range myPeers {
-			if otherPeers[p] == myPeers[myPeer] {
+			if otherPeers[p].Port == myPeers[myPeer].Port {
 				exists = true
+				if otherPeers[p].State.Version > myPeers[myPeer].State.Version {
+					myPeers[myPeer].State = otherPeers[p].State
+				}
 			}
 		}
-		if !exists {
+		if !exists && otherPeers[p].Port != 0 && otherPeers[p].Port != selfState.Port {
 			myPeers = append(myPeers, otherPeers[p])
 		}
 	}
--- main.go
+++ main.go
@@ -7,24 +7,34 @@ import (
 	"os"
 	"strconv"
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	if len(os.Args) != 3 {
-		fmt.Println("Incorrect number of args, expects 2.")
+	if len(os.Args) < 2 || len(os.Args) > 3 {
+		fmt.Println("Incorrect number of args, expects 1 or 2.")
 		os.Exit(1)
 	}
-	myPort := os.Args[1]
-	peerPort, err := strconv.Atoi(os.Args[2])
+	myPort, err := strconv.Atoi(os.Args[1])
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
+	var peerPort int
+	if len(os.Args) == 3 {
+		peerPort, err = strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	} else {
+		peerPort = 0
+	}
+
 	var mc api.MikeCoin
 	mc.Initialize()
-	gossip.Initialize(peerPort)
+	gossip.Initialize(myPort, peerPort)
 
 
 	mux := http.DefaultServeMux
@@ -33,5 +43,11 @@ func main() {
 	mux.HandleFunc("/transfers", mc.Transfers)
 	mux.HandleFunc("/status", mc.GetStatus)
 	mux.HandleFunc("/gossip", gossip.HandleGossip)
-	http.ListenAndServe(":" + myPort, mux)
+	go http.ListenAndServe(":" + strconv.Itoa(myPort), mux)
+	for {
+		time.Sleep(5000 * time.Millisecond)
+		gossip.ChangeMovie()
+		gossip.Gossip()
+		gossip.PrintPeers()
+	}
 }
